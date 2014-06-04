a385a393f7a160728a4ace03c6bed6b92f27d2a5
Fernando <fernando.rodriguez.adesis@gmail.com>
2014-06-04 17:00:45 +0200

    GREEN

--- src/test/java/com/adesis/cursoTDD/parser/ParserTest.java
+++ src/test/java/com/adesis/cursoTDD/parser/ParserTest.java
@@ -14,15 +14,18 @@ public class ParserTest {
 		List<String> results = parse("cocinero");
 		assertThat(results.get(0)).isEqualTo("COCINERO");
 	}
-	
+
 	@Test
 	public void eliminar_articulo_de_query() {
 		List<String> results = parse("un cocinero");
-		assertThat("COCINERO").isEqualTo(results.get(0));
+		assertThat(results.get(0)).isEqualTo("COCINERO");
 	}
-	
-	public List<String> parse(String query){
+
+	public List<String> parse(String query) {
 		List<String> results = new ArrayList<String>();
+		if (query.contains("un ")) {
+			query = query.substring(3);
+		}
 		results.add(query.toUpperCase());
 		return results;
 	}
