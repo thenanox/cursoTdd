557660dbfe2e4301e3f202c202f23c344c5db886
Fernando <fernando.rodriguez.adesis@gmail.com>
2014-06-04 17:06:40 +0200

    REFACTOR

--- src/test/java/com/adesis/cursoTDD/parser/ParserTest.java
+++ src/test/java/com/adesis/cursoTDD/parser/ParserTest.java
@@ -21,10 +21,14 @@ public class ParserTest {
 		assertThat(results.get(0)).isEqualTo("COCINERO");
 	}
 
+	private static final String[] EXCLUSIONS_LIST = {"un ","uno ","una ","unos ","unas ","el ","la ","los ","las "};
+	
 	public List<String> parse(String query) {
 		List<String> results = new ArrayList<String>();
-		if (query.contains("un ")) {
-			query = query.substring(3);
+		for(String exclusion: EXCLUSIONS_LIST){
+			if(query.contains(exclusion)){
+				query = query.replace(exclusion, "");
+			}
 		}
 		results.add(query.toUpperCase());
 		return results;
